9b980c6e8b6856e2c8f164d700c1c11a09474556
Jacques Mahe <jacques.mahe@gmail.com>
2023-08-31 20:10:42 -0400

    - Cleaned text:    -> Now using top to align the characters to the base line    -> Using global char sizes    -> Starting characters at ASCII code 30

--- python_server/test_font.py
+++ python_server/test_font.py
@@ -14,13 +14,14 @@ from PIL import Image
 import time
 
 
-fontfile = "Vera.ttf"
+fontfile = "FreeMono.ttf"
 #fontfile = r'C:\source\resource\fonts\gnu-freefont_freesans\freesans.ttf'
 
 class CharacterSlot:
     def __init__(self, texture, glyph):
         self.texture = texture
         self.textureSize = (glyph.bitmap.width, glyph.bitmap.rows)
+        self.top = glyph.bitmap_top
 
         if isinstance(glyph, freetype.GlyphSlot):
             self.bearing = (glyph.bitmap_left, glyph.bitmap_top)
@@ -31,14 +32,14 @@ class CharacterSlot:
         else:
             raise RuntimeError('unknown glyph type')
 
-def _get_rendering_buffer(xpos, ypos, w, h, zfix=0.0):
+def _get_rendering_buffer(xpos, ypos, w, h, top, zfix=0.0):
     return np.asarray([
-        xpos,     ypos - h, 0, 0,
-        xpos,     ypos,     0, 1,
-        xpos + w, ypos,     1, 1,
-        xpos,     ypos - h, 0, 0,
-        xpos + w, ypos,     1, 1,
-        xpos + w, ypos - h, 1, 0
+        xpos,     ypos + (h-top) - h, 0, 0,
+        xpos,     ypos + (h-top),     0, 1,
+        xpos + w, ypos + (h-top),     1, 1,
+        xpos,     ypos + (h-top) - h, 0, 0,
+        xpos + w, ypos + (h-top),     1, 1,
+        xpos + w, ypos + (h-top) - h, 1, 0
     ], np.float32)
 
 
@@ -76,6 +77,9 @@ Characters = dict()
 VBO = None
 VAO = None
 
+CHAR_SIZE_W = 12*64
+CHAR_SIZE_H = 15*64
+
 def ortho_matrix(left, right, bottom, top, near, far):
     """
     Returns an orthographic projection matrix.
@@ -98,9 +102,12 @@ def initialize():
     global Characters
     global VBO
     global VAO
+    global CHAR_SIZE_W
+    global CHAR_SIZE_H
 
     VAO = glGenVertexArrays(1)
     glBindVertexArray(VAO)
+    glEnable(GL_MULTISAMPLE)
 
     #compiling shaders
     vertexshader = shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER)
@@ -122,18 +129,22 @@ def initialize():
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
 
     face = freetype.Face(fontfile)
-    face.set_char_size(28*64, 20*64)
+    face.set_char_size(CHAR_SIZE_W, CHAR_SIZE_H)
 
     #load first 128 characters of ASCII set
-    for i in range(0,128):
+    for i in range(30, 128):
         face.load_char(chr(i))
         glyph = face.glyph
-        
+        if chr(i) > ' ':
+            print ("Loading character '%c': width: %d, height: %d, left: %d, top: %d" % (chr(i), glyph.bitmap.width, glyph.bitmap.rows, glyph.bitmap_left, glyph.bitmap_top))
+
         #generate texture
         texture = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, texture)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, glyph.bitmap.width, glyph.bitmap.rows, 0,
-                     GL_RED, GL_UNSIGNED_BYTE, glyph.bitmap.buffer)
+        glTexImage2D(
+            GL_TEXTURE_2D, 0, GL_RED, 
+            glyph.bitmap.width, glyph.bitmap.rows, 0,
+            GL_RED, GL_UNSIGNED_BYTE, glyph.bitmap.buffer)
 
         #texture options
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
@@ -142,7 +153,7 @@ def initialize():
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
         #now store character for later use
-        Characters[chr(i)] = CharacterSlot(texture,glyph)
+        Characters[chr(i)] = CharacterSlot(texture, glyph)
         
     glBindTexture(GL_TEXTURE_2D, 0)
 
@@ -156,16 +167,19 @@ def initialize():
     glBindBuffer(GL_ARRAY_BUFFER, 0)
     glBindVertexArray(0)
     
-def render_text(window,text,x,y,scale,color):
+def render_text(window, text, x, y, scale, color):
     global shaderProgram
     global Characters
     global VBO
     global VAO
+    global CHAR_SIZE_W
+    global CHAR_SIZE_H
     
     face = freetype.Face(fontfile)
-    face.set_char_size(28*64,20*64)
-    glUniform3f(glGetUniformLocation(shaderProgram, "textColor"),
-                color[0]/255,color[1]/255,color[2]/255)
+    face.set_char_size(CHAR_SIZE_W, CHAR_SIZE_H)
+    glUniform3f(glGetUniformLocation(
+        shaderProgram, "textColor"),
+        color[0]/255,color[1]/255,color[2]/255)
                
     glActiveTexture(GL_TEXTURE0)
     
@@ -176,9 +190,9 @@ def render_text(window,text,x,y,scale,color):
     for c in text:
         ch = Characters[c]
         w, h = ch.textureSize
-        w = w*scale
-        h = h*scale
-        vertices = _get_rendering_buffer(x,y,w,h)
+        w = w * scale
+        h = h * scale
+        vertices = _get_rendering_buffer(x, y, w, h, ch.top * scale)
 
         #render glyph texture over quad
         glBindTexture(GL_TEXTURE_2D, ch.texture)
@@ -190,7 +204,7 @@ def render_text(window,text,x,y,scale,color):
         #render quad
         glDrawArrays(GL_TRIANGLES, 0, 6)
         #now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-        x += (ch.advance>>6)*scale
+        x += (ch.advance >> 6) * scale
 
     glBindVertexArray(0)
     glBindTexture(GL_TEXTURE_2D, 0)
@@ -200,12 +214,12 @@ def render_text(window,text,x,y,scale,color):
     
 def main():
     glfw.init()
-    #glfwWindowHint(GLFW_SAMPLES, 4)
 
     glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
     glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
     glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
     glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+    glfw.window_hint(glfw.SAMPLES, 4)
 
     window = glfw.create_window(640, 640,"EXAMPLE PROGRAM",None,None)    
     glfw.make_context_current(window)
@@ -215,7 +229,7 @@ def main():
         glfw.poll_events()
         glClearColor(0,0,0,1)
         glClear(GL_COLOR_BUFFER_BIT)
-        render_text(window, 'Hello World - This is a text you need to read', 20, 40, 1, (255, 0, 0))
+        render_text(window, 'Hello World -_.^', 20, 40, 1, (255, 255, 255))
 
     glfw.terminate()
 
