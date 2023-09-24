- Fixed initial Torus rotation by recalculating the vertices      -> We can now rotate around any of the axis alost indepedently. However, once rotated around an axis, rotating around other will cause additional rotation aroubd the first axis. We will need to add compennsation.      -> Added Torus boundaries - Reduced mouse double-click time to 0.2ms (from 0.5ms) - Replace "cube index" with "geometry index" since we are also using tori. - Moved update_mouse_pos outside Mesh class and into ServerFramework class so that it can apply to other objects like tori,     -> Changed self.selection_cube into self.selection_geometry and added tori to it     -> Added EditMode.ROTATION that show tori around the objects.     -> Added different sensitivities for mouse scale, move and rotation. - Disabled auto-rotation when an object is selected. - Added update_boundaries to Mesh class; the update is now done in Mesh constructor instead of LoadMesh constructor.

python_server/Framework/ServerFramework.py
@@ -25,11 +25,14 @@ class ServerFramework(PyOGLApp):
         self.picking_object = None  
         self.edit_mode = EditMode.NOT_SELECTED
         self.selected_geometry = Selection(None, -1)
-        self.selection_cubes = []
+        self.selection_geometry = []
         self.selection_axis = None
         self.fonts = dict()
         self.geometry3D = dict()
         self.geometry2D = dict()
+        self.mouse_scale_sensitivity = 0.01
+        self.mouse_move_sensitivity = 0.01
+        self.mouse_rotation_sensitivity = 0.1
 
     def initialize_3D_space(self):
         self.shaders = {
@@ -40,22 +43,29 @@ class ServerFramework(PyOGLApp):
             'selection' : Shader("shaders/picking_vertices.vs", "shaders/picking_frags.vs"),
             'geometry 2D' : Shader("shaders/geometry2D_vertices.vs", "shaders/geometry2D_frags.vs") }
         self.picking_object = PickingObject(self.get_shader('selection'))
-        self.create_selection_cubes()
+        self.create_format_selection_geometry()
 
-    def create_selection_cubes(self):
-        colors = [[1, 0, 0], [0, 1, 0], [0, 0, 1],
-                  [1, 0, 0], [0, 1, 0], [0, 0, 1]]
-        for cube_index in range(len(colors)):
-            cube = LoadMesh(
-                self.get_shader('colored'),
-                "models/cube.obj", None,
-                location=(0.0, 0.0, 0.0),
-                color=colors[cube_index],
-                gl_draw_type=GL_TRIANGLES,
-                scale=(0.05, 0.05, 0.05),
-                rotation=(0, 0, 0),
-                move_rotation=(0, 0, 0))
-            self.selection_cubes.append(cube)
+    def create_format_selection_geometry(self):
+        cubes_colors = [[1, 0, 0], [0, 1, 0], [0, 0, 1],
+                       [1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        for cube_index in range(len(cubes_colors)):
+            self.selection_geometry.append(
+                LoadMesh(self.get_shader('colored'),
+                        "models/cube.obj", None,
+                        location=(0.0, 0.0, 0.0),
+                        color=cubes_colors[cube_index],
+                        gl_draw_type=GL_TRIANGLES,
+                        scale=(0.05, 0.05, 0.05),
+                        rotation=(0, 0, 0),
+                        move_rotation=(0, 0, 0)))
+        tori_colors = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        tori_rotation = np.array([[0.0, 90.0, 0.0], [90.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+        for torus_index in range(len(tori_colors)):
+            self.selection_geometry.append(
+                Torus(self.get_shader('colored'), None, 
+                    1.0, 0.02, color=tori_colors[torus_index],
+                    initial_rotation=tori_rotation[torus_index],
+                    move_rotation=(0.0, 0.0, 0.0)))
         
     def add_geometry3D(self, name, geometry, selectable=True):
         # First load, then append
@@ -89,14 +99,43 @@ class ServerFramework(PyOGLApp):
         for _, geometry in self.geometry2D.items():
             geometry.update_display_size(display_width, display_height)
 
+    def update_mouse_pos(self, selected_object, edit_mode, delta_x, delta_y):
+        if self.selected_geometry.geometry_index != -1:
+            geometry_index = self.selected_geometry.geometry_index
+            value_to_modify = None
+            if edit_mode == EditMode.POSITION:
+                value_to_modify = selected_object.location
+                mouse_sensitivity = self.mouse_move_sensitivity
+            elif edit_mode == EditMode.SCALE:
+                value_to_modify = selected_object.scale
+                mouse_sensitivity = self.mouse_scale_sensitivity
+            elif edit_mode == EditMode.ROTATION:
+                value_to_modify = selected_object.rotation
+                mouse_sensitivity = self.mouse_rotation_sensitivity
+            else:
+                raise Exception("Not a valid editing mode!")
+            delta_pos = (delta_x - delta_y) * mouse_sensitivity
+            if geometry_index == 0 or geometry_index == 3 or geometry_index == 6:
+                value_to_modify = (value_to_modify[0] + delta_pos, value_to_modify[1], value_to_modify[2])
+            elif geometry_index == 1 or geometry_index == 4 or geometry_index == 7:
+                value_to_modify = (value_to_modify[0], value_to_modify[1] + delta_pos, value_to_modify[2])
+            elif geometry_index == 2 or geometry_index == 5 or geometry_index == 8:
+                value_to_modify = (value_to_modify[0], value_to_modify[1], value_to_modify[2] + delta_pos)
+            if edit_mode == EditMode.POSITION:
+                selected_object.location = value_to_modify
+            elif edit_mode == EditMode.SCALE:
+                selected_object.scale = value_to_modify
+            elif edit_mode == EditMode.ROTATION:
+                selected_object.rotation = value_to_modify
+
     def mouse_pos_callback(self, window, xpos, ypos):
         if self.track_mouse:
             if self.selected_geometry.name == None:
                 super().mouse_pos_callback(window, xpos, ypos)
             else:
-                self.get_geometry3D(self.selected_geometry.name).update_mouse_pos(
-                    self.selected_geometry, self.edit_mode, 
-                    xpos-self.last_mouse_pos[0], ypos-self.last_mouse_pos[1])
+                self.update_mouse_pos(
+                    self.get_geometry3D(self.selected_geometry.name),
+                    self.edit_mode, xpos-self.last_mouse_pos[0], ypos-self.last_mouse_pos[1])
                 if self.selection_axis != None:
                     self.selection_axis.location = self.get_geometry3D(self.selected_geometry.name).location
                     self.selection_axis.scale = self.get_geometry3D(self.selected_geometry.name).scale
@@ -110,18 +149,18 @@ class ServerFramework(PyOGLApp):
             selection = self.picking_object.MousePick(
                 self.max_resolution[0], self.max_resolution[1],
                 location[0], location[1],
-                self.geometry3D, self.selection_cubes,
+                self.geometry3D, self.selection_geometry,
                 self.camera, self.selected_geometry)
         else:
             selection = self.picking_object.MousePick(
                 self.display_width, self.display_height,
                 location[0], location[1],
-                self.geometry3D, self.selection_cubes,
+                self.geometry3D, self.selection_geometry,
                 self.camera, self.selected_geometry)
         #print("selection: object is " + str(selection.object_index) +
         #      ", fragment is " + str(selection.primitive_index) +
         #      ", cube is " + str(selection.cube_index))
-        if selection.cube_index == -1: # i.e. when none of the "editing" objects are selected
+        if selection.geometry_index == -1: # i.e. when none of the "editing" objects are selected
             if self.selected_geometry.name != None:
                 self.get_geometry3D(self.selected_geometry.name).selected = False
             if selection.name != None and selection.name != self.selected_geometry.name:
@@ -131,18 +170,27 @@ class ServerFramework(PyOGLApp):
                 self.selection_axis = Axis(
                     self.get_shader('colored'),
                     location=object.location,
-                    boundaries=offset_object_boundaries(object.boundaries, boundaries_offset),
+                    boundaries=offset_object_boundaries(object.get_boundaries(), boundaries_offset),
                     scale=object.scale)
                 self.selected_geometry = selection
             elif selection.name != None and self.edit_mode == EditMode.POSITION:
                 object = self.get_geometry3D(selection.name)
+                object.selected = True
+                self.edit_mode = EditMode.SCALE
                 self.selection_axis = Axis(
                     self.get_shader('colored'),
                     location=object.location,
-                    boundaries=offset_object_boundaries(object.boundaries, boundaries_offset),
+                    boundaries=offset_object_boundaries(object.get_boundaries(), boundaries_offset),
                     scale=object.scale,
                     rotation=object.rotation)
-                self.edit_mode = EditMode.SCALE
+            elif selection.name != None and self.edit_mode == EditMode.SCALE:
+                object = self.get_geometry3D(selection.name)
+                object.selected = True
+                self.edit_mode = EditMode.ROTATION
+                self.update_rotation_torus(self.selection_geometry[6], object, "x")
+                self.update_rotation_torus(self.selection_geometry[7], object, "y")
+                self.update_rotation_torus(self.selection_geometry[8], object, "z")
+                self.selection_axis = None
             else:
                 self.edit_mode = EditMode.NOT_SELECTED
                 self.selected_geometry = Selection(None, -1, -1)
@@ -152,7 +200,7 @@ class ServerFramework(PyOGLApp):
 
     def display_selection_cubes(self, cube, object, axis, negative):
         cube.location = copy.deepcopy(object.location)
-        boundaries = offset_object_boundaries(object.boundaries, boundaries_offset)
+        boundaries = offset_object_boundaries(object.get_boundaries(), boundaries_offset)
         scale = object.scale
         cube.rotation = object.rotation
         delta_location = np.array([0.0, 0.0, 0.0, 1.0])
@@ -181,6 +229,20 @@ class ServerFramework(PyOGLApp):
                          cube.location[2]+delta_location[2])
         cube.draw(self.camera, self.lights)
 
+    def update_rotation_torus(self, torus, object, axis):
+        boundaries = offset_object_boundaries(object.get_boundaries(), boundaries_offset)
+        max_object_diameter = (boundaries[3] - boundaries[0]) * object.scale[0]
+        if (boundaries[4] - boundaries[1]) * object.scale[1] > max_object_diameter:
+            max_object_diameter = (boundaries[4] - boundaries[1]) * object.scale[1]
+        if (boundaries[5] - boundaries[2]) * object.scale[2] > max_object_diameter:
+            max_object_diameter = (boundaries[5] - boundaries[2]) * object.scale[2]
+        torus.resize(max_object_diameter / 2)
+
+    def display_tori(self, torus, object, axis):
+        torus.location = object.location
+        torus.rotation = object.rotation
+        torus.draw(self.camera, self.lights)
+
     def draw(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         display_width, display_height = glfw.get_framebuffer_size(self.window)
@@ -188,12 +250,17 @@ class ServerFramework(PyOGLApp):
             geometry.draw(self.camera, self.lights)
         if self.selected_geometry.name != None:
             object = self.get_geometry3D(self.selected_geometry.name) # Shallow copy
-            self.display_selection_cubes(self.selection_cubes[0], object, "x", True)
-            self.display_selection_cubes(self.selection_cubes[1], object, "y", True)
-            self.display_selection_cubes(self.selection_cubes[2], object, "z", True)
-            self.display_selection_cubes(self.selection_cubes[3], object, "x", False)
-            self.display_selection_cubes(self.selection_cubes[4], object, "y", False)
-            self.display_selection_cubes(self.selection_cubes[5], object, "z", False)
+            if self.edit_mode == EditMode.POSITION or self.edit_mode == EditMode.SCALE:
+                self.display_selection_cubes(self.selection_geometry[0], object, "x", True)
+                self.display_selection_cubes(self.selection_geometry[1], object, "y", True)
+                self.display_selection_cubes(self.selection_geometry[2], object, "z", True)
+                self.display_selection_cubes(self.selection_geometry[3], object, "x", False)
+                self.display_selection_cubes(self.selection_geometry[4], object, "y", False)
+                self.display_selection_cubes(self.selection_geometry[5], object, "z", False)
+            elif self.edit_mode == EditMode.ROTATION:
+                self.display_tori(self.selection_geometry[6], object, "x")
+                self.display_tori(self.selection_geometry[7], object, "y")
+                self.display_tori(self.selection_geometry[8], object, "z")
             if self.selection_axis != None:
                 self.selection_axis.draw(self.camera, self.lights)
         for _, geometry in self.geometry2D.items():

python_server/Framework/glapp/Torus.py
@@ -9,6 +9,7 @@ class Torus(Mesh):
             inner_radius=0.5,
             slices=20, # How many outer rings
             loops=40, # How many inner rings
+            initial_rotation=(0.0, 0.0, 0.0),
             color=(1.0, 1.0, 1.0),
             gl_draw_type=GL_TRIANGLES,
             boundaries=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
@@ -22,10 +23,33 @@ class Torus(Mesh):
         self.inner_radius = inner_radius
         self.slices = slices
         self.loops = loops
+        self.initial_rotation = initial_rotation
+        self.color = [color[0], color[1], color[2]]
+        vertices, normals, vertex_uvs, colors = self.create_torus()
+        super().__init__(
+            shader_program,
+            vertices,
+            image_filename,
+            vertex_normals=normals,
+            vertex_uvs=vertex_uvs,
+            vertex_colors=colors,
+            gl_draw_type=gl_draw_type,
+            location=location,
+            scale=scale,
+            rotation=rotation,
+            move_rotation=move_rotation,
+            move_location=move_location)
+
+    def create_torus(self):
         raw_vertices = []
         raw_normals = []
         raw_vertex_uvs = []
 
+        transformation_mat = identity_mat()
+        transformation_mat = rotateA(transformation_mat, self.initial_rotation[0], (1, 0, 0))
+        transformation_mat = rotateA(transformation_mat, self.initial_rotation[1], (0, 1, 0))
+        transformation_mat = rotateA(transformation_mat, self.initial_rotation[2], (0, 0, 1))
+    
         for slice in range(self.slices+1):
             v = slice / self.slices
             slice_angle = v * 2 * np.pi
@@ -37,7 +61,7 @@ class Torus(Mesh):
                 #   x=(R+r·cos(v))cos(w)
                 #   y=(R+r·cos(v))sin(w)
                 #   z=r.sin(v)
-                u = loop / self.loops
+                u = loop / self.loops 
                 loop_angle = u * 2 * np.pi
                 cos_loops = np.cos(loop_angle)
                 sin_loops = np.sin(loop_angle)
@@ -46,12 +70,18 @@ class Torus(Mesh):
                 y = slice_radius * sin_loops
                 z = self.inner_radius * sin_slices
 
-                raw_vertices.append([x, y, z])
-                raw_normals.append([
+                vertex = np.array([x, y, z, 1])
+                vertex = np.matmul(transformation_mat, vertex)
+                raw_vertices.append([vertex[0], vertex[1], vertex[2]])
+                normal = np.array([
                     cos_slices*cos_loops,
                     sin_loops*cos_slices, 
-                    sin_slices])
-                raw_vertex_uvs.append([u, v]);
+                    sin_slices,
+                    1
+                ])
+                normal = np.matmul(transformation_mat, normal)
+                raw_normals.append([normal[0], normal[1], normal[2]])
+                raw_vertex_uvs.append([u, v])
         
         vertices = []
         normals = []
@@ -87,25 +117,33 @@ class Torus(Mesh):
                 vertex_uvs.append(raw_vertex_uvs[v2+1])
 
                 for k in range(6):
-                    colors.append([color[0], color[1], color[2]])
+                    colors.append(self.color)
 
                 v1 = v1 + 1
                 v2 = v2 + 1
-        
-        super().__init__(
-            shader_program,
-            vertices,
-            image_filename,
-            vertex_normals=normals,
-            vertex_uvs=vertex_uvs,
-            vertex_colors=colors,
-            gl_draw_type=gl_draw_type,
-            location=location,
-            scale=scale,
-            rotation=rotation,
-            move_rotation=move_rotation,
-            move_location=move_location)
+        return vertices, normals, vertex_uvs, colors
 
+    def resize(self, outer_radius, inner_radius=None, slices=None, loops=None):
+        self.outer_radius = outer_radius
+        if inner_radius is not None:
+            self.inner_radius = inner_radius
+        if slices is not None:
+            self.slices = slices
+        if loops is not None:
+            self.loops = loops
+        self.vertices, self.vertex_normals, self.vertex_uvs, self.vertex_colors = self.create_torus()
+        self.length = len(self.vertices)
+        for i in range(len(self.vertices)):
+            self.vertex_indices.append([i//3, 0])
+        self.load()
+        self.update_boundaries()
 
-        
-        
+    def update_boundaries(self):
+        self.boundaries = [
+            -self.outer_radius - self.inner_radius,
+            -self.outer_radius - self.inner_radius,
+            -self.inner_radius,
+            self.outer_radius + self.inner_radius,
+            self.outer_radius + self.inner_radius,
+            self.inner_radius]
+        return self.boundaries